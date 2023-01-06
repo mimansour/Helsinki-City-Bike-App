bb4e77dc466078ae446dfb7ab66661ad7ca8814c
Mira Mansour <mira@mansour.fi>
2023-01-06 14:01:51 +0200

    feat: add journey-data validation

--- lib/utils/csv.ts
+++ lib/utils/csv.ts
@@ -1,13 +1,14 @@
 import * as fs from 'fs'
 import path from 'path'
-import { parse, Callback } from 'csv-parse'
+import { parse } from 'csv-parse/sync'
+import Validator from 'fastest-validator'
 
 export const getCsvFile = (dir: string, filePath: string) => {
   const dirPath = path.join(process.cwd(), dir)
   return fs.readFileSync(`${dirPath}/${filePath}`, 'utf8')
 }
 
-type BikeJourney = {
+export type BikeJourney = {
   departureDate: string
   returnDate: string
   departureStationId: string
@@ -18,22 +19,22 @@ type BikeJourney = {
   duration: number // in seconds
 }
 
-export const parseCsv = (
-  headers: string[],
-  csvFile: string,
-  parseCallback: Callback
-) => {
-  parse(
-    csvFile,
-    {
-      delimiter: ',',
-      columns: headers,
-    },
-    parseCallback
+export const parseCsv = (headers: string[], csvFile: string): BikeJourney[] =>
+  parse(csvFile, {
+    delimiter: ',',
+    columns: headers,
+  })
+
+export const parseJourniesFromCsv = () => {
+  const files = ['2021-05.csv', '2021-06.csv', '2021-07.csv']
+
+  return files.reduce(
+    (journies, file) => [...journies, ...parseFileJournies(file)],
+    [] as BikeJourney[]
   )
 }
 
-export const parseJournies = () => {
+export const parseFileJournies = (fileName: string) => {
   const headers = [
     'departureDate',
     'returnDate',
@@ -45,11 +46,26 @@ export const parseJournies = () => {
     'duration',
   ]
 
-  const file = getCsvFile('data/journies', '2021-05.csv')
-  parseCsv(headers, file, (error, result: BikeJourney[]) => {
-    if (error) {
-      console.error(error)
-    }
-    console.log(result)
-  })
+  const file = getCsvFile('data/journies', fileName)
+  const result = parseCsv(headers, file)
+
+  return result.filter(journeyIsValid)
 }
+
+const v = new Validator()
+
+const journeySchema = {
+  departureDate: { type: 'date', convert: true },
+  returnDate: { type: 'date', convert: true },
+  departureStationId: { type: 'string', min: 3 },
+  departureStationName: { type: 'string' },
+  returnStationId: { type: 'string', min: 3 },
+  returnStationName: { type: 'string' },
+  duration: { type: 'number', min: 10, convert: true },
+  distance: { type: 'number', min: 10, convert: true },
+}
+
+const validateJourney = v.compile(journeySchema)
+
+const journeyIsValid = (journey: BikeJourney) =>
+  typeof validateJourney(journey) === 'boolean'
