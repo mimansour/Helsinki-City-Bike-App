90e744e44ec381f7b6a9533bd39f231eafd17cf5
Mira Mansour <mira@mansour.fi>
2023-01-31 21:14:40 +0200

    style: refactor journies & stations table styles

--- pages/stations/index.tsx
+++ pages/stations/index.tsx
@@ -2,6 +2,7 @@ import { getAllStations } from 'lib/db/station'
 import { BikeStation } from '@/lib/types/station'
 import StationsTable, { createColumnHelper } from 'components/StationsTable'
 import Link from 'next/link'
+import ChevronRightIcon from 'components/ChevronRightIcon'
 
 const columnHelper = createColumnHelper<BikeStation>()
 
@@ -9,10 +10,13 @@ const columns = [
   columnHelper.accessor('nameFi', {
     cell: (info) => (
       <Link
-        className="hover:text-amber-400"
+        className="hover:text-amber-500 text-gray-600 flex flex-row gap-x-2 items-center"
         href={`/stations/${info.row.original.stationId}`}
       >
         {info.getValue()}
+        <span className="w-3 h-3 flex justify-center">
+          <ChevronRightIcon />
+        </span>
       </Link>
     ),
     header: () => <span>Name</span>,
@@ -32,7 +36,10 @@ export async function getServerSideProps() {
 function Stations({ stations }: { stations: BikeStation[] }) {
   return (
     <div className="gap-y-6 mx-10 flex flex-col items-center">
-      <h2 className="font-bold text-2xl">Stations</h2>
+      <h2 className="font-bold text-3xl uppercase">Stations</h2>
+      <p>
+        Click the station name to get more information about the given station.
+      </p>
       <StationsTable<BikeStation> data={stations} columns={columns} />
     </div>
   )
