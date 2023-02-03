5231d99dc477762bbd404bb869bf991996640e81
Mira Mansour <mira@mansour.fi>
2023-02-03 16:36:50 +0200

    style: improve responsiveness

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react'
 
 export default function Layout(props: PropsWithChildren) {
   return (
-    <div className="bg-neutral-50 text-stone-700 font-sans min-h-screen">
+    <div className="bg-neutral-50 text-stone-700 font-sans h-full min-h-screen min-w-fit">
       <Navbar />
       <main className="min-h-[75vh] py-10">{props.children}</main>
       <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import PrimaryLinkButton from 'components/general/PrimaryLinkButton'
 
 const Home = () => (
   <div className="pt-20 flex flex-col justify-center items-center gap-y-6 text-center">
-    <h1 className="text-8xl max-w-4xl uppercase font-bold bg-clip-text text-transparent bg-gradient-to-r from-amber-500 to-red-400">
+    <h1 className="md:text-8xl text-5xl max-w-4xl uppercase font-bold bg-clip-text text-transparent bg-gradient-to-r from-amber-500 to-red-400">
       Helsinki City Bike App
     </h1>
-    <p className="w-3/5 pt-10 text-xl text-gray-600">
+    <p className="w-3/5 pt-10 md:text-xl text-lg text-gray-600">
       Helsinki City Bike App displays statistics about the bicycle stations and
       journeys in the Helsinki metropolitan area.
     </p>
